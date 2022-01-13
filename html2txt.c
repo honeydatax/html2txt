fb21c415b1f569a0daed0c661d6ddd5b48b9ac85
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-01-13 11:00:33 +0000

    Add files via upload

--- html2txt.c
+++ html2txt.c
@@ -4,43 +4,56 @@
 
 int main(int argc, char *argv[]){
 	int n;
-	char c[2080];
+	char *c;
 	char *cc;
 	char *ccc;
 	char *cccc;
-	FILE *f;
+	FILE* f;
 	int style=0;
 	int r=0;
-		f=fopen("/dev/stdin","r");
-		if (f!=NULL){
-			style=0;
-			while(feof(f)!=1){
-				strcpy(c,"");
-				fgets(c,2070,f);
-				r=0;
-				ccc=c;
-				while(r!=1){
-					cc=strstr(ccc,"<");
-					if(cc!=NULL){
-						cccc=ccc;
-						ccc=cc+1;
-						cc[0]=0;
-					}else{
-						cccc=ccc;
-						r=1;
-					}
-						if(style==1)printf("%s",cccc);
-						cc=strstr(ccc,"body");
-						if(cc-ccc<3)style=1;
-						cc=strstr(ccc,"BODY");
-						if(cc-ccc<3)style=1;
-						cc=strstr(ccc,">");
-						if(cc!=NULL){
-							ccc=cc+1;
-							r=0;
-						}else{
-							r=1;
+		if(argc>1){
+			f=fopen(argv[1],"r");
+			if (f!=NULL){
+				fseek(f,0,SEEK_END);
+				n=ftell(f);
+				if(n<5000008){
+					fseek(f,0,SEEK_SET);
+					c=malloc(n+5);
+					if (c!=NULL){
+						style=0;
+						c[0]=0;
+						fread(c,n,1,f);
+						c[n]=0;
+						r=0;
+						ccc=c;
+						while(r!=1){
+							cc=strstr(ccc,"<");
+							if(cc!=NULL){
+								cccc=ccc;
+								ccc=cc+1;
+								cc[0]=0;
+							}else{
+								cccc=ccc;
+								r=1;
+							}
+							if(style==1)printf("%s",cccc);
+							cc=strstr(ccc,"BODY");
+							if(cc!=NULL && cc-ccc<3)style=1;
+							cc=strstr(ccc,"body");
+							if(cc!=NULL && cc-ccc<3)style=1;
+							cc=strstr(ccc,"Body");
+							if(cc!=NULL && cc-ccc<3)style=1;
+
+							cc=strstr(ccc,">");
+							if(cc!=NULL){
+								ccc=cc+1;
+								r=0;
+							}else{
+								r=1;
+							}
 						}
+						free(c);
+					}
 				}
 			}
 		}
--- html2txt.sh
+++ html2txt.sh
@@ -6,5 +6,5 @@ tr -d '\n' < $1.txt > /tmp/1
 tr -d '\r' < /tmp/1  > $1.txt
 sed -i 's/<br>/\n/g' $1.txt
 sed -i 's/<p>/\n/g' $1.txt 
-cat $1.txt | ./html2txt
+./html2txt $1.txt
 rm /tmp/1
